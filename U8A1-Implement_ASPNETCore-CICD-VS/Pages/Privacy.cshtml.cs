a23f39bc53789509d5112582427a1d2bc955b59a
Johnny <Johnny@DESKTOP-B7UA8QF>
2023-12-13 23:39:06 -0500

    Add project files.

---
+++ U8A1-Implement_ASPNETCore-CICD-VS/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace U8A1_Implement_ASPNETCore_CICD_VS.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
